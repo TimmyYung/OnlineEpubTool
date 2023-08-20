75313335445c299257be32f5d04880d9d7af29b8
TimmyYung <timmyyyung2002@gmail.com>
2023-08-20 11:05:21 -0400

    Changed homepage design

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            entry.target.classList.toggle('show', entry.isIntersecting);
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
